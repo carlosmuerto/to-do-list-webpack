6e798e6588b1cdd085c3d6b2545e5f86b0dbdb27
Carlos Vivas <carlosmuerto@gmail.com>
2022-08-09 19:06:04 -0400

    Add html content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import _ from 'lodash';
 import './style.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
-function component() {
-  const element = document.createElement('div');
+const init = () => { };
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener('load', init);

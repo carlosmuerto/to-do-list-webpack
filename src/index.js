c9785e3b809e4304c41c6794f4fb61180a644658
Carlos Vivas <carlosmuerto@gmail.com>
2022-08-10 17:03:49 -0400

    Add LocalStorage

--- src/index.js
+++ src/index.js
@@ -4,32 +4,6 @@ import ToDoList from './modules/to-do-list.js';
 import { PopulateTaskList, removeTaskElement } from './modules/to-do-dom-element.js';
 
 const toDoList = new ToDoList();
-const exmpleToDo = [
-  {
-    description: 'toDo 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'toDo 2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'toDo 4 done',
-    completed: true,
-    index: 7,
-  },
-  {
-    description: 'toDo 3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'toDo last',
-    completed: false,
-  },
-];
 
 const removeTaskButtonEvent = (e) => {
   e.preventDefault();
@@ -56,7 +30,7 @@ const refreshListBtn = (e) => {
 };
 
 const init = () => {
-  toDoList.init(exmpleToDo);
+  toDoList.init();
   PopulateTaskList(toDoList);
   document.getElementById('clear-task-btn').addEventListener('click', removeTaskButtonEvent);
   document.getElementById('add-task-form').addEventListener('submit', addTaskToTodoList);
--- src/modules/to-do-list.js
+++ src/modules/to-do-list.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import ToDo from './to-do.js';
+import storageAvailable from './localStorageTools.js';
 
 class ToDoList {
   constructor(list = []) {
@@ -10,6 +11,7 @@ class ToDoList {
     if (!(toDo instanceof ToDo)) throw Error(`${toDo} is Not a ToDo object`);
     this.tasks.push(toDo);
     this.tasks = _.sortBy(this.tasks, ['index', 'description']);
+    this.refreshTaskIndex();
   }
 
   generateNextIndex() {
@@ -33,6 +35,7 @@ class ToDoList {
 
   refreshTaskIndex() {
     this.tasks.forEach((task, index) => { task.index = index; });
+    this.updateStoreFormData();
   }
 
   deleteDoneTasks() {
@@ -41,11 +44,25 @@ class ToDoList {
     });
   }
 
-  init(listTodo) {
-    listTodo.forEach((todo) => {
-      this.addToDo(todo.description, todo.completed, todo.index);
-    });
-    this.refreshTaskIndex();
+  getStoreFormData() {
+    const obj = JSON.parse(localStorage.getItem('ToDoList-tasks-data'));
+    if (obj) {
+      obj.tasks.forEach((todo) => {
+        this.addToDo(todo.description, todo.completed, todo.index);
+      });
+    }
+  }
+
+  updateStoreFormData() {
+    if (!storageAvailable()) return;
+    localStorage.setItem(
+      'ToDoList-tasks-data',
+      JSON.stringify(this),
+    );
+  }
+
+  init() {
+    this.getStoreFormData();
   }
 }
 

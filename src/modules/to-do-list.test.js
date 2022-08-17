03f9dbf98fba3469fc2edc1e8d3b0c2d64f67620
addis <betsega23@gmail.com>
2022-08-17 23:30:33 +0300

    Unit test for Delte function

--- src/modules/to-do-list.test.js
+++ src/modules/to-do-list.test.js
@@ -3,6 +3,8 @@ const TodoList = require('./to-do-list.js');
 // Arrenge
 
 const toDoListForAdd = new TodoList([], false);
+const toDoListDelete = new TodoList([], false);
+const expectedtoDoListForDeleteIndex = 0;
 const expectedtoDoListForAdd = {
   tasks: [
     {
@@ -17,11 +19,15 @@ const expectedtoDoListForAdd = {
 // act
 
 toDoListForAdd.addToDo('test task', true);
-
+toDoListDelete.addToDo('ffdagf', true);
+toDoListDelete.delete(0);
 // accert
 
 describe('Test for TodoList Class', () => {
   test('Add Task to the List', () => {
     expect(toDoListForAdd).toEqual(expectedtoDoListForAdd);
+  });
+  test('Delete task from list', () => {
+    expect(toDoListDelete.tasks).toEqual([]);
   });
 });
